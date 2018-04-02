9483a47489c2a76bf57bdf36de679c44a02df995
dan nguyen <ddnguyen0702@hotmail.com>
2018-04-03 03:29:32 +0700

    update

--- CPUTEMPW/Main.Designer.cs
+++ CPUTEMPW/Main.Designer.cs
@@ -28,27 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.cpuTempBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tempDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cpuTempBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AutoGenerateColumns = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nameDataGridViewTextBoxColumn,
+            this.tempDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.cpuTempBindingSource;
             this.dataGridView1.Location = new System.Drawing.Point(12, 12);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(776, 318);
+            this.dataGridView1.Size = new System.Drawing.Size(288, 283);
             this.dataGridView1.TabIndex = 0;
             // 
-            // Form1
+            // cpuTempBindingSource
+            // 
+            this.cpuTempBindingSource.DataSource = typeof(CPUTEMPW.CpuTemp);
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            // 
+            // tempDataGridViewTextBoxColumn
+            // 
+            this.tempDataGridViewTextBoxColumn.DataPropertyName = "Temp";
+            this.tempDataGridViewTextBoxColumn.HeaderText = "Temp";
+            this.tempDataGridViewTextBoxColumn.Name = "tempDataGridViewTextBoxColumn";
+            // 
+            // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(314, 312);
             this.Controls.Add(this.dataGridView1);
-            this.Name = "Form1";
+            this.Name = "Main";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Main_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cpuTempBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -56,6 +84,9 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tempDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource cpuTempBindingSource;
     }
 }
 
--- CPUTEMPW/Main.cs
+++ CPUTEMPW/Main.cs
@@ -16,5 +16,10 @@ namespace CPUTEMPW
         {
             InitializeComponent();
         }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
