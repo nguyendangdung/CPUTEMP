55318605ee05d785b18bf8559dbecac391a5da9e
dan nguyen <ddnguyen0702@hotmail.com>
2018-04-03 03:24:28 +0700

    update

--- CPUTEMP/Program.cs
+++ CPUTEMP/Program.cs
@@ -1,15 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenHardwareMonitor.Hardware;
 
 namespace CPUTEMP
 {
-    class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
+            var updateVisitor = new UpdateVisitor();
+            var computer = new Computer();
+            computer.Open();
+            computer.CPUEnabled = true;
+            computer.Accept(updateVisitor);
+            var i = 0;
+            while (i < 100)
+            {
+                i++;
+                GetSystemInfo(computer);
+            }
+            computer.Close();
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+        static void GetSystemInfo(Computer computer)
+        {
+            
+            foreach (var t in computer.Hardware)
+            {
+                if (t.HardwareType != HardwareType.CPU) continue;
+                foreach (var t1 in t.Sensors)
+                {
+                    if (t1.SensorType == SensorType.Temperature)
+                        Console.WriteLine(t1.Name + ":" + t1.Value.ToString() + "\r");
+                }
+            }
         }
     }
 }
