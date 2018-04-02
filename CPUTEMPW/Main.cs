02710fca070531dac96bdc5aeb743bc21a67ce4f
dan nguyen <ddnguyen0702@hotmail.com>
2018-04-03 03:33:39 +0700

    update

--- CPUTEMPW/Main.cs
+++ CPUTEMPW/Main.cs
@@ -7,17 +7,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OpenHardwareMonitor.Hardware;
 
 namespace CPUTEMPW
 {
+    public class UpdateVisitor : IVisitor
+    {
+        public void VisitComputer(IComputer computer)
+        {
+            computer.Traverse(this);
+        }
+        public void VisitHardware(IHardware hardware)
+        {
+            hardware.Update();
+            foreach (IHardware subHardware in hardware.SubHardware) subHardware.Accept(this);
+        }
+        public void VisitSensor(ISensor sensor) { }
+        public void VisitParameter(IParameter parameter) { }
+    }
     public partial class Main : Form
     {
+        private readonly UpdateVisitor _updateVisitor;
+        private readonly Computer _computer;
         public Main()
         {
             InitializeComponent();
+            _updateVisitor = new UpdateVisitor();
+            _computer = new Computer();
+            _computer.Open();
+            _computer.CPUEnabled = true;
+            _computer.Accept(_updateVisitor);
         }
 
-        private void Main_Load(object sender, EventArgs e)
+        private async void Main_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private async Task DoSomething()
         {
 
         }
